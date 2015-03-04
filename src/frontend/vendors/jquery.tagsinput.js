7876bd300f68df0d6c15e02400c2b1a18cd2c275
Stefano Ortisi <stefanoortisi@gmail.com>
2015-03-04 11:48:26 +0100

    fix on tagsinput garbage collector

--- src/frontend/vendors/jquery.tagsinput.js
+++ src/frontend/vendors/jquery.tagsinput.js
@@ -75,14 +75,17 @@
 
   $.fn.destroy_tagsinput = function() { 
   	for( k in tags_callbacks ){
-  		if( tags_callbacks[k].onAddTag != undefined){
-  			tags_callbacks[k].onAddTag = null;
-  		}
-  		if( tags_callbacks[k].onChange != undefined){
-  			tags_callbacks[k].onChange = null;
-  		}
-  		if( tags_callbacks[k].onRemoveTag != undefined){
-  			tags_callbacks[k].onRemoveTag = null;
+  		if( tags_callbacks[k] != undefined ){
+  			
+	  		if( tags_callbacks[k].onAddTag != undefined){
+	  			tags_callbacks[k].onAddTag = null;
+	  		}
+	  		if( tags_callbacks[k].onChange != undefined){
+	  			tags_callbacks[k].onChange = null;
+	  		}
+	  		if( tags_callbacks[k].onRemoveTag != undefined){
+	  			tags_callbacks[k].onRemoveTag = null;
+	  		}
   		}
   		tags_callbacks[k] = null;
   	}
