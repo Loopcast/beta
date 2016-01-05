ba26ad7ab67895546e76f60d757af7e9fac1e8a6
hems <hems.inlet@gmail.com>
2016-01-05 20:10:30 +0000

    improving google analytics integration

--- public/js/ga.js
+++ public/js/ga.js
@@ -6,8 +6,6 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-70238750-1', 'auto');
 ga('send', 'pageview');
 
-alert 'ga created'
-
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-70238750-1']);
 _gaq.push(['_trackPageview']);
@@ -17,5 +15,4 @@ _gaq.push(['_trackPageview']);
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 
-  alert 'analytics created'
 })();
