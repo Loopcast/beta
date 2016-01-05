b221b9f573101be8294edbbd588815e4dd6b1977
hems <hems.inlet@gmail.com>
2016-01-05 20:08:52 +0000

    trying out

--- public/js/ga.js
+++ public/js/ga.js
@@ -6,7 +6,7 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 ga('create', 'UA-70238750-1', 'auto');
 ga('send', 'pageview');
 
-
+alert 'ga created'
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-70238750-1']);
@@ -16,4 +16,6 @@ _gaq.push(['_trackPageview']);
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+
+  alert 'analytics created'
 })();
